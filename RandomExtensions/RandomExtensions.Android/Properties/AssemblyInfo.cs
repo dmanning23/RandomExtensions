3fc9dfb7224eaf0f6c54003ffb588b1dab9c8a52
Dan Manning <dmanning23@hotmail.com>
2016-04-03 22:01:05 -0500

    moved to separate nuget packages

--- RandomExtensions/RandomExtensions.Android/Properties/AssemblyInfo.cs
+++ RandomExtensions/RandomExtensions.Android/Properties/AssemblyInfo.cs
@@ -1,26 +1,20 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RandomExtensions")]
+[assembly: AssemblyTitle("RandomExtensions.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RandomExtensions")]
+[assembly: AssemblyProduct("RandomExtensions.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("bc0c4267-fcd6-4f4c-be52-4c7310e4ac69")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ RandomExtensions/buildNugetPackages.ps1
@@ -0,0 +1,3 @@
+nuget pack .\RandomExtensions.Android\RandomExtensions.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\RandomExtensions.DesktopGL\RandomExtensions.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\RandomExtensions.iOS\RandomExtensions.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
