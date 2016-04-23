1e530c538467628af600b394cfb5a653b40ac6df
Dan Manning <dmanning23@hotmail.com>
2016-04-23 00:28:15 -0500

    added winuni project

--- RandomExtensions/buildNugetPackages.ps1
+++ RandomExtensions/buildNugetPackages.ps1
@@ -1,3 +1,5 @@
 nuget pack .\RandomExtensions.Android\RandomExtensions.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack .\RandomExtensions.DesktopGL\RandomExtensions.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack .\RandomExtensions.iOS\RandomExtensions.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\RandomExtensions.WindowsUniversal\RandomExtensions.WindowsUniversal.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
