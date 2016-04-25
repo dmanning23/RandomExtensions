9976cf03cb8dc2f214f9cb6ed309b240ad601938
Dan Manning <dmanning23@hotmail.com>
2016-04-24 19:41:43 -0500

    fixed nuget package

--- RandomExtensions/buildNugetPackages.ps1
+++ RandomExtensions/buildNugetPackages.ps1
@@ -1,2 +1,2 @@
-nuget pack .\RandomExtensionsnuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\RandomExtensions.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 nuget push *.nupkg
