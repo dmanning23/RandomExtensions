a72ffdc119f76bcc96f161aec40feed5f8c9b5df
Dan Manning <dmanning23@hotmail.com>
2017-05-10 17:59:49 -0500

    Moved code into shared project

--- RandomExtensions/buildNugetPackages.ps1
+++ RandomExtensions/buildNugetPackages.ps1
@@ -1,2 +1,3 @@
+rm *.nupkg
 nuget pack .\RandomExtensions.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget push *.nupkg
+nuget push -source https://www.nuget.org -NonInteractive *.nupkg
