5413806765f460f2634702b6e570369bb033fd1b
Dan Manning <dmanning@oceaneering.com>
2021-11-02 16:02:23 -0500

    Added bridge project

--- RandomExtensions/buildNugetPackages.ps1
+++ RandomExtensions/buildNugetPackages.ps1
@@ -1,4 +1,5 @@
 rm *.nupkg
 nuget pack .\RandomExtensions.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\RandomExtensions.Bridge.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 cp *.nupkg C:\Projects\Nugets\
 nuget push -source https://www.nuget.org -NonInteractive *.nupkg
