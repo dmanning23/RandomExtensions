96f7d4491b9933aa5b0ed162df013bd4551fe4b6
Dan Manning <dmanning@oceaneering.com>
2023-06-30 13:51:13 -0500

    reset to 3.7.1

--- RandomExtensions/buildNugetPackages.ps1
+++ RandomExtensions/buildNugetPackages.ps1
@@ -1,5 +1,4 @@
 rm *.nupkg
 nuget pack .\RandomExtensions.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\RandomExtensions.Bridge.nuspec -IncludeReferencedProjects -Prop Configuration=Release
 cp *.nupkg C:\Projects\Nugets\
 nuget push -source https://www.nuget.org -NonInteractive *.nupkg
